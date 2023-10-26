2f96ebec0fbf7687cc1112a656fd7f9fe7ceb58e
LiuJi0Fen04 <205844372@qq.com>
2023-10-26 18:35:27 +0800

    RBTree rotate

--- code/src/chapter13.cpp
+++ code/src/chapter13.cpp
@@ -12,4 +12,29 @@
 // ******************************************************************
 
 
+/**
+ *      |                        |
+ *      x      left rotate       y
+ *     / \    ------------->    / \
+ *    α   y   <-------------   x   γ
+ *       / \   right rotate   / \
+ *      β   γ                α   β
+ */
+void leftRotate(RBTree_Node* root, RBTree_Node* x)
+{
+    RBTree_Node* y;
+    y = x->rchild;
+    x->rchild = y->lchild;
+    if( y->lchild != NULL) // 这里要不要改成sentinel T.nil
+        y->lchild->parent = x;
+    y->parent = x->parent;
+    if( x->parent == NULL)
+        root = y;
+    else if(x == x->parent->lchild)
+        x->parent->lchild = y;
+    else
+        x->parent->rchild = y;
+    y->lchild = x;
+    x->parent = y;
+}
 
